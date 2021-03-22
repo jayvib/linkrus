7276b89ef1b756625d7a183385c83761c3ce8483
jaysonv <danv@itquoter.com>
2021-03-22 12:24:18 +0800

    Implement the grpc server

--- api/linkgraphapi/server.go
+++ api/linkgraphapi/server.go
@@ -1,7 +1,95 @@
 package linkgraphapi
 
-import "linkrus/linkgraph/graph"
+import (
+	"context"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/google/uuid"
+	"linkrus/api/linkgraphapi/proto"
+	"linkrus/linkgraph/graph"
+	"time"
+)
 
-func NewLinkGraphServer(g graph.Graph) interface{} {
-	return nil
+var _ proto.LinkGraphServer = (*LinkGraphServer)(nil)
+
+type LinkGraphServer struct {
+	g graph.Graph
+	proto.UnimplementedLinkGraphServer
+}
+
+func NewLinkGraphServer(g graph.Graph) *LinkGraphServer {
+	return &LinkGraphServer{g: g}
+}
+
+func (l *LinkGraphServer) UpsertLink(_ context.Context, req *proto.Link) (*proto.Link, error) {
+	var (
+		err error
+
+		// Initialize a link
+		link = graph.Link{
+			// with id
+			ID: uuidFromBytes(req.Uuid),
+			// with url
+			URL: req.Url,
+		}
+	)
+
+	// Convert from ptypes.Timestamp to time
+	link.RetrievedAt, err = ptypes.Timestamp(req.RetrievedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	// Upsert the link
+	if err = l.g.UpsertLink(&link); err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func (l *LinkGraphServer) UpsertEdge(_ context.Context, req *proto.Edge) (*proto.Edge, error) {
+	edge := graph.Edge{
+		ID:  uuidFromBytes(req.Uuid),
+		Src: uuidFromBytes(req.SrcUuid),
+		Dst: uuidFromBytes(req.DstUuid),
+	}
+
+	if err := l.g.UpsertEdge(&edge); err != nil {
+		return nil, err
+	}
+
+	req.Uuid = edge.ID[:]
+	req.SrcUuid = edge.Src[:]
+	req.DstUuid = edge.Dst[:]
+	req.UpdatedAt = timeToProto(edge.UpdatedAt)
+
+	return req, nil
+}
+
+func (l *LinkGraphServer) RemoveStaleEdges(ctx context.Context, query *proto.RemoveStaleEdgesQuery) (*empty.Empty, error) {
+	panic("implement me")
+}
+
+func (l *LinkGraphServer) Links(r *proto.Range, server proto.LinkGraph_LinksServer) error {
+	panic("implement me")
+}
+
+func (l *LinkGraphServer) Edges(r *proto.Range, server proto.LinkGraph_EdgesServer) error {
+	panic("implement me")
+}
+
+func uuidFromBytes(b []byte) uuid.UUID {
+	if len(b) != 16 {
+		return uuid.Nil
+	}
+	var dst uuid.UUID
+	copy(dst[:], b)
+	return dst
+}
+
+func timeToProto(t time.Time) *timestamp.Timestamp {
+	ts, _ := ptypes.TimestampProto(t)
+	return ts
 }
--- api/linkgraphapi/server_test.go
+++ api/linkgraphapi/server_test.go
@@ -1,12 +1,17 @@
 package linkgraphapi_test
 
 import (
+	"context"
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/test/bufconn"
 	gc "gopkg.in/check.v1"
+	"linkrus/api/linkgraphapi"
 	"linkrus/api/linkgraphapi/proto"
 	"linkrus/linkgraph/graph"
+	"linkrus/linkgraph/store/memory"
+	"net"
+	"time"
 )
 
 var _ = gc.Suite(new(ServerTestSuite))
@@ -16,35 +21,142 @@ var maxUUID = uuid.MustParse("ffffffff-ffff-ffff-ffff-ffffffffffff")
 type ServerTestSuite struct {
 	g graph.Graph
 
+	// Network listener
 	netListener *bufconn.Listener
-	grpcSrv     *grpc.Server
+	// Grpc server
+	grpcSrv *grpc.Server
 
+	// Grpc client connection
 	cliConn *grpc.ClientConn
-	cli     proto.LinkGraphClient
+
+	// Client
+	cli proto.LinkGraphClient
 }
 
 func (s *ServerTestSuite) SetUpTest(c *gc.C) {
-	//s.g = memory.NewInMemoryGraph()
-	//
-	//s.netListener = bufconn.Listen(1024)
-	//
-	//s.grpcSrv = grpc.NewServer()
-	//
-	//proto.RegisterLinkGraphServer(s.grpcSrv, linkgraphapi.NewLinkGraphServer(s.g))
-	//
-	//go func() {
-	//	err := s.grpcSrv.Serve(s.netListener)
-	//	c.Assert(err, gc.NotNil)
-	//}()
-	//
-	//var err error
-	//s.cliConn, err = grpc.Dial(
-	//	"bufnet",
-	//	grpc.WithContextDialer(func(context.Context, string)(net.Conn, error) { return s.netListener.Dial() }),
-	//	grpc.WithInsecure(),
-	//)
-	//
-	//c.Assert(err, gc.IsNil)
-	//
-	//s.cli = proto.NewLinkGraphClient(s.cliConn)
+	s.g = memory.NewInMemoryGraph()
+
+	s.netListener = bufconn.Listen(1024)
+
+	s.grpcSrv = grpc.NewServer()
+
+	proto.RegisterLinkGraphServer(s.grpcSrv, linkgraphapi.NewLinkGraphServer(s.g))
+
+	go func() {
+		err := s.grpcSrv.Serve(s.netListener)
+		c.Assert(err, gc.NotNil)
+	}()
+
+	var err error
+	s.cliConn, err = grpc.Dial(
+		"bufnet",
+		grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) { return s.netListener.Dial() }),
+		grpc.WithInsecure(),
+	)
+
+	c.Assert(err, gc.IsNil)
+
+	s.cli = proto.NewLinkGraphClient(s.cliConn)
+}
+
+func (s *ServerTestSuite) TearDownTest(_ *gc.C) {
+	_ = s.cliConn.Close()
+	s.grpcSrv.Stop()
+	_ = s.netListener.Close()
+}
+
+func (s *ServerTestSuite) TestInsertLink(c *gc.C) {
+	now := time.Now().Truncate(time.Second).UTC()
+	req := &proto.Link{
+		Url:         "http://example.com",
+		RetrievedAt: mustEncodeTimestamp(c, now),
+	}
+	res, err := s.cli.UpsertLink(context.TODO(), req)
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.Uuid, gc.Not(gc.DeepEquals), req.Uuid, gc.Commentf("UUID not assigned to new link"))
+	c.Assert(mustDecodeTimestamp(c, res.RetrievedAt), gc.Equals, now)
+}
+
+func (s *ServerTestSuite) TestUpdateLink(c *gc.C) {
+
+	// Insert first a link
+	link := &graph.Link{URL: "http://example.com"}
+	c.Assert(s.g.UpsertLink(link), gc.IsNil)
+
+	// Update the link
+	now := time.Now().Truncate(time.Second).UTC()
+	req := &proto.Link{
+		Uuid:        link.ID[:],
+		Url:         "http://example.com",
+		RetrievedAt: mustEncodeTimestamp(c, now),
+	}
+
+	// Do request to the GRPC server
+	res, err := s.cli.UpsertLink(context.TODO(), req)
+	c.Assert(err, gc.IsNil)
+
+	// Assert the id
+	c.Assert(req.Uuid, gc.DeepEquals, res.Uuid, gc.Commentf("UUID for existing link modified"))
+
+	// Assert the URL
+	c.Assert(req.Url, gc.Equals, res.Url, gc.Commentf("URL not updated"))
+
+	// Assert the timestamp
+	c.Assert(mustDecodeTimestamp(c, res.RetrievedAt), gc.Equals, now)
+}
+
+func (s *ServerTestSuite) TestInsertEdge(c *gc.C) {
+	// Add two links to the graph
+	src := &graph.Link{URL: "http://example.com"}
+	dst := &graph.Link{URL: "http://foo.com"}
+
+	// In order to set the ID of above links
+	c.Assert(s.g.UpsertLink(src), gc.IsNil)
+	c.Assert(s.g.UpsertLink(dst), gc.IsNil)
+
+	// Create an Edge
+	req := &proto.Edge{
+		SrcUuid: src.ID[:],
+		DstUuid: dst.ID[:],
+	}
+
+	res, err := s.cli.UpsertEdge(context.TODO(), req)
+
+	c.Assert(err, gc.IsNil)
+	c.Assert(res.Uuid[:], gc.Not(gc.DeepEquals), req.Uuid[:], gc.Commentf("UUID not assigned to new edge"))
+	c.Assert(res.DstUuid[:], gc.DeepEquals, req.DstUuid[:])
+	c.Assert(res.SrcUuid[:], gc.DeepEquals, req.SrcUuid[:])
+	c.Assert(res.UpdatedAt, gc.Not(gc.Equals), 0)
+}
+
+func (s *ServerTestSuite) TestUpdateEdge(c *gc.C) {
+	// Create a 2 new link for source the destination
+	src := &graph.Link{URL: "http://example.com"}
+	dst := &graph.Link{URL: "http://foo.com"}
+	c.Assert(s.g.UpsertLink(src), gc.IsNil)
+	c.Assert(s.g.UpsertLink(dst), gc.IsNil)
+
+	// Create an edge
+	edge := &graph.Edge{
+		Src: src.ID,
+		Dst: dst.ID,
+	}
+	c.Assert(s.g.UpsertEdge(edge), gc.IsNil)
+
+	// Update the edge
+	req := &proto.Edge{
+		Uuid:    edge.ID[:],
+		SrcUuid: src.ID[:],
+		DstUuid: dst.ID[:],
+	}
+
+	res, err := s.cli.UpsertEdge(context.TODO(), req)
+	c.Assert(err, gc.IsNil)
+	// Should be equal
+	c.Assert(res.Uuid, gc.DeepEquals, req.Uuid, gc.Commentf("UUID for existing edge modified"))
+	c.Assert(res.DstUuid[:], gc.DeepEquals, req.DstUuid[:])
+	c.Assert(res.SrcUuid[:], gc.DeepEquals, req.SrcUuid[:])
+
+	// Assert the date. And the res.UpdatedAt must be after the edge.UpdatedAt
+	c.Assert(mustDecodeTimestamp(c, res.UpdatedAt).After(edge.UpdatedAt), gc.Equals, true)
 }
